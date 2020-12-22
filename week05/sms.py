4b158e38dd32a11e04fc8ea0419ff75e6eb4f9ec
Eric Fang <gzeureka@gmail.com>
2020-12-23 07:57:25 +0800

    重构，将每分钟限制发送次数作为参数传入
    # Body (detailed explanation)
    # number (bug/issue/tracking if applicable)

--- week05/sms.py
+++ week05/sms.py
@@ -7,13 +7,13 @@ import time
 import redis
 
 
-def can_send(list):
-    # 未超过 5 次，可以发送
-    if len(list) < 5:
+def can_send(list, limit: int):
+    # 未超过 limit 次，可以发送
+    if len(list) < limit:
         return True
 
-    # 获取倒数第 5 次的发送时间
-    t = float(list[-5].decode())
+    # 获取倒数第 limit 次的发送时间
+    t = float(list[-limit].decode())
     now = time.time()
     # 时间差是否小于 1 分钟
     if now - t < 60:
@@ -22,17 +22,17 @@ def can_send(list):
         return True
 
 
-
 def sendsms(client, telephone_number: str, contents: str, key=None):
     # 用 Redis 列表记录每个手机号的最后 5 次发送时间
     # 请实现每分钟相同手机号最多发送五次功能, 超过 5 次提示调用方,1 分钟后重试稍后
+    limit = 5
     list = client.lrange(telephone_number, 0, -1)
-    if can_send(list):
+    if can_send(list, limit):
         # 本次发送的时间加到时间列表末尾
         client.rpush(telephone_number, time.time())
-        # 发送时间列表表头出队，减少存储空间 
-        if len(list) >= 4:
-            client.ltrim(telephone_number, -5, -1)
+        # 发送时间列表表头出队，减少存储空间
+        if len(list) >= limit-1:
+            client.ltrim(telephone_number, -limit, -1)
         print("发送成功", telephone_number, contents)
     else:
         print(telephone_number, "1 分钟内发送次数超过 5 次, 请等待 1 分钟")
@@ -55,6 +55,6 @@ if __name__ == '__main__':
     if len(sys.argv) < 2:
         print('Please specify a phone number')
         exit(1)
-    
+
     telephone_number = sys.argv[1]
     main(telephone_number)
