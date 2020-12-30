23f950c4fe39f57713e01e42cd1b10218fbe0c8d
Eric Fang <gzeureka@gmail.com>
2020-12-30 21:48:49 +0800

    zoo.py

--- week06/zoo.py
+++ week06/zoo.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
-# 
+#
 # 背景：在使用 Python 进行《我是动物饲养员》这个游戏的开发过程中，有一个代码片段要求定义动物园、动物、猫、狗四个类。
-# 
+#
 # 这个类可以使用如下形式为动物园增加一只猫：
-# 
+#
 # if __name__ == '__main__':
 #     # 实例化动物园
 #     z = Zoo('时间动物园')
@@ -13,12 +13,66 @@
 #     z.add_animal(cat1)
 #     # 动物园是否有猫这种动物
 #     have_cat = hasattr(z, 'Cat')
-# 
+#
 # 具体要求：
 #   定义“动物”、“猫”、“狗”、“动物园”四个类，动物类不允许被实例化。
 #   动物类要求定义“类型”、“体型”、“性格”、“是否属于凶猛动物”四个属性，是否属于凶猛动物的判断标准是：“体型 >= 中等”并且是“食肉类型”同时“性格凶猛”。
 #   猫类要求有“叫声”、“是否适合作为宠物”以及“名字”三个属性，其中“叫声”作为类属性，除凶猛动物外都适合作为宠物，猫类继承自动物类。狗类属性与猫类相同，继承自动物类。
 #   动物园类要求有“名字”属性和“添加动物”的方法，“添加动物”方法要实现同一只动物（同一个动物实例）不能被重复添加的功能。
+
+from abc import ABC
+
+
+def Zoo(object):
+    def __init__(self, name):
+        self.name = name
+        self.animals = []
+
+    def add_animal(self, animal):
+        # 是否已有同一只动物
+        for a in self.animals:
+            if animal is a:
+                return
+        # 添加动物
+        self.animals.append(animal)
+
+    def has_animal(self, type_name):
+        canonical_type_name = f'__main__.{type_name}'
+        for a in self.animals:
+            if canonical_type_name == type(a):
+                return True
+        return False
+
+# body_type '小'，'中'，'大'
+# character '性格'，'温顺'，'凶猛'
+def Animal(ABC):
+    def __init__(self, diet, body_type, character):
+        self.diet = diet
+        self.body_type = body_type
+        self.character = character
+
+    # 是否凶猛动物
+    @property
+    def is_fierce(self):
+        self.body_type in [
+            '中', '大'] and self.diet == '肉食' and self.character == '凶猛'
+
+
+def Cat(Animal):
+    voice = 'Meow'
+
+    def __init(self, name, diet, body_type, character):
+        super().__init__(self, diet, body_type, character)
+        self.name = name
+
+
+def Dog(Animal):
+    voice = 'Woof'
+
+    def __init(self, name, diet, body_type, character):
+        super().__init__(self, diet, body_type, character)
+        self.name = name
+
 
 if __name__ == '__main__':
     pass
